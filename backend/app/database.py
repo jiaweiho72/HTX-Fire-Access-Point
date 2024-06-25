00478aeb7dcef2ba3d6b8b1108bdf9d11ef91c3b
jiaweiho72 <99948407+jiaweiho72@users.noreply.github.com>
2024-06-25 14:24:00 +0800

    docker setup

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,8 +6,14 @@ from dotenv import load_dotenv
 
 load_dotenv()  # Load environment variables from .env file
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_HOST = "db"
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 # SQLALCHEMY_DATABASE_URL = 'postgresql://guest2:90108501@localhost:5432/map'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
